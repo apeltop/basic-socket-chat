80c87582e2cd331e251fb0e2487795b395cefc95
LeeSangHwee <tkdgnl56@naver.com>
2024-10-22 20:22:11 +0900

    edit

--- server/index.js
+++ server/index.js
@@ -29,12 +29,12 @@ io.on("connection", (socket) => {
     id: `${socket.id}-${randNum}`,
     content: "사용자가 연결되었습니다",
     sender: `사용자 ${randNum}`,
-    timestamp: new Date(),
+    timestamp: new Date().toLocaleString(),
   };
   // 서버 로그
   console.log(`사용자 ${socket.id}-${randNum}가 연결되었습니다`);
-  // 클라에 로그인 메시지 전송
-  socket.broadcast.emit("SEND_MESSAGE", { loginMessage });
+  // 메시지를 보낸 클라를 제외한 모든 클라에게 로그인 메시지 전송
+  socket.broadcast.emit("SEND_MESSAGE", JSON.stringify(loginMessage));
   // 여기서부터 채팅 ----------------------------------------------------------------
   // 클라로부터 메시지 수신
   socket.on("SEND_MESSAGE", (msg) => {
@@ -42,14 +42,14 @@ io.on("connection", (socket) => {
       id: `${socket.id}-${randNum}`,
       content: msg,
       sender: `사용자 ${randNum}`,
-      timestamp: new Date(),
+      timestamp: new Date().toLocaleString(),
     };
     // 서버 로그
     console.log(`${socket.id}: `, msg);
     // message 에 채팅 데이터 저장
     messages.push(message);
-    // 모든 클라에게 메시지 전송
-    socket.broadcast.emit("SEND_MESSAGE", { message });
+    // 메시지를 보낸 클라를 포함한 모든 클라에게 메시지 전송
+    io.emit("SEND_MESSAGE", JSON.stringify(message));
   });
   // 여기서부터 채팅방 연결 해제 ----------------------------------------------------------------
   socket.on("disconnect", () => {
@@ -57,18 +57,18 @@ io.on("connection", (socket) => {
       id: `${socket.id}-${randNum}`,
       content: "사용자가 연결을 끊었습니다",
       sender: `사용자 ${randNum}`,
-      timestamp: new Date(),
+      timestamp: new Date().toLocaleString(),
     };
     // 서버 로그
     console.log(`사용자 ${socket.id}-${randNum}연결을 끊었습니다`);
-    // 클라에 로그아웃 메시지 전송
-    socket.broadcast.emit("SEND_MESSAGE", { logOutMessage });
+    // 메시지를 보낸 클라를 제외한 모든 클라에게 로그아웃 메시지 전송
+    socket.broadcast.emit("SEND_MESSAGE", JSON.stringify(logOutMessage));
   });
 });
 
 // 채팅 기록 조회
 app.get("/messages", (req, res) => {
-  res.status(200).json(messages);
+  res.status(200).json(JSON.stringify(messages));
 });
 
 app.use(express.static(path.join(path.resolve(), "public")));
