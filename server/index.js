b30052135ec908ef26c233e93d04594433449998
LeeSangHwee <tkdgnl56@naver.com>
2024-10-25 16:45:02 +0900

    줄바꿈

--- server/index.js
+++ server/index.js
@@ -31,10 +31,13 @@ io.on("connection", (socket) => {
     sender: `사용자 ${randNum}`,
     timestamp: new Date().toLocaleString(),
   };
+
   // 서버 로그
   console.log(`사용자 ${socket.id}-${randNum}가 연결되었습니다`);
+
   // 메시지를 보낸 클라를 제외한 모든 클라에게 로그인 메시지 전송
   socket.broadcast.emit("SEND_MESSAGE", JSON.stringify(loginMessage));
+
   // 여기서부터 채팅 ----------------------------------------------------------------
   // 클라로부터 메시지 수신
   socket.on("SEND_MESSAGE", (msg) => {
@@ -44,13 +47,17 @@ io.on("connection", (socket) => {
       sender: `사용자 ${randNum}`,
       timestamp: new Date().toLocaleString(),
     };
+
     // 서버 로그
     console.log(`${socket.id}: `, msg);
+
     // message 에 채팅 데이터 저장
     messages.push(message);
+
     // 메시지를 보낸 클라를 포함한 모든 클라에게 메시지 전송
     io.emit("SEND_MESSAGE", JSON.stringify(message));
   });
+
   // 여기서부터 채팅방 연결 해제 ----------------------------------------------------------------
   socket.on("disconnect", () => {
     const logOutMessage = {
@@ -59,8 +66,10 @@ io.on("connection", (socket) => {
       sender: `사용자 ${randNum}`,
       timestamp: new Date().toLocaleString(),
     };
+
     // 서버 로그
     console.log(`사용자 ${socket.id}-${randNum}연결을 끊었습니다`);
+
     // 메시지를 보낸 클라를 제외한 모든 클라에게 로그아웃 메시지 전송
     socket.broadcast.emit("SEND_MESSAGE", JSON.stringify(logOutMessage));
   });
